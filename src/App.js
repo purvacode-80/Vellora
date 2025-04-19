d3d967e4dd11f7ecbe384b29adf7ca415de1db30
purvacode-80 <purvaw06@gmail.com>
2025-04-19 18:31:44 +0530

    ProtectedRoutes added to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick-theme.css";
 import './App.css';
 import Testimonials from "./Components/testimonials";
 import Faqquestion from "./Components/faqquestion";
 import Footer from "./Components/footer";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProtectedRoutes from "./Security/ProtectedRoute";
+import { AuthProvider } from "./Security/AuthContext";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
 
 function App() {
   return (
     <>
-     <Testimonials/>
+    <BrowserRouter>
+    <AuthProvider>
+     {/* <Testimonials/>
      <Faqquestion/>
-     <Footer/>
+     <Footer/> */}
+      <Routes>
+          <Route path="/home" element={<ProtectedRoutes> <Faqquestion /> </ProtectedRoutes>}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+      </Routes>
+    </AuthProvider>
+    </BrowserRouter>
     </>
   );
 }
