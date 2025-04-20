e0288529a066f2655087a5f81f36804cad3956a8
snehamore555 <snehamore2172006@gmail.com>
2025-04-20 16:06:44 +0530

    save changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import Home from "../src/Home";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 import Testimonials from "./Components/testimonials";
 import Faqquestion from "./Components/faqquestion";
 import Footer from "./Components/footer";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ProtectedRoutes from "./Security/ProtectedRoute";
-import { AuthProvider } from "./Security/AuthContext";
-import Login from "./Components/Login";
-import Register from "./Components/Register";
+import Home from "./Components/Home"
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <AuthProvider>
-     {/* <Testimonials/>
+     <Home/>
+     <Testimonials/>
      <Faqquestion/>
-     <Footer/> */}
-      <Routes>
-          <Route path="/home" element={<ProtectedRoutes> <Home /> <Testimonials /> <Faqquestion /> <Footer /> </ProtectedRoutes>}/>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/register" element={<Register />}/>
-      </Routes>
-    </AuthProvider>
-    </BrowserRouter>
+     <Footer/>
     </>
   );
 }
